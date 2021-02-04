b2c33117f17da0e953957b6102a7816d691435b1
mounikamoni9999 <71550057+mounikamoni9999@users.noreply.github.com>
2021-02-04 15:17:22 +0530

    task2
    my name is mounika

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int main()
 {
     printf("Hello world!\n my name is pr.");
     printf("\n my name is abhisar.");
-     printf("\n my name is mounika.");
+     printf("\n my name is mounika");
     return 0;
 }
