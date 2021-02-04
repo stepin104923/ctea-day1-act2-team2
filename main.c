58deb21347d438deadc45e89bdd305d380033406
Sai Bhargav <69418503+stepin105359@users.noreply.github.com>
2021-02-04 15:48:49 +0530

    added my name

--- main.c
+++ main.c
@@ -5,10 +5,9 @@ int main()
     printf("Hello world!\n my name is pr.");
     printf("\n my name is abhisar.");
      printf("\n my name is mounika");
-    printf("Hello world!\nvghvh");
     printf("Hello World!\n I'm Sanjana Gs");
     printf("My name is suryudu\n");
-    
+    printf("My name is bhargav\n");
     printf("\n I'm Gouthamm G");
      printf("\n my name is madura");
      printf("\n my name is geethika");
